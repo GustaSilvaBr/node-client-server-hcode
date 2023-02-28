bf8ba22e6dc759e7689171547e2431c5ac4a1067
Gustavo Silva <gustavo.silva@ear.local>
2023-02-28 07:08:56 -0300

    :sparkles: feat: first request to a API restfull

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
+
+const restify = require('restify-clients');
+const assert = require('assert');
 var express = require('express');
 var router = express.Router();
 
+const serverToRequest = 'http://localhost:4000';
+
+const client = restify.createJsonClient({
+  url: serverToRequest
+});
+
+// client.basicAuth('$login','$password'); -> restFull auth
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (requestExpress, responseExpress, next) {
+  //res.send('respond with a resource');Invés de devolver uma requisição
+  //para o client que fez a chamada iremos realizar uma requisição para outro server
+
+  client.get('/users', function (err, requestRestify, responseRestify, obj) {
+    assert.ifError(err);
+
+    responseExpress.end(JSON.stringify(obj, null, 2));
+  });
 });
 
 module.exports = router;
